5c99628ffba30ad91ae4f918fdea2a120e7e0ec9
Tim Oxley <secoif@gmail.com>
2014-10-03 14:59:36 +0800

    Use browser-launcher2.

--- test/spawn.js
+++ test/spawn.js
@@ -2,14 +2,20 @@ var test = require('tap').test;
 var spawn = require('child_process').spawn;
 
 test('spawn', function (t) {
-  var ps = spawn(__dirname + '/../bin/bin.js');
-  
+  var ps = spawn(__dirname + '/../bin/bin.js', ['--browser', 'chrome']);
+
   ps.stdout.on('data', function (data) {
     t.equal(data.toString(), 'foo\n');
-    ps.kill();
-    t.end();
   });
-  ps.stderr.on('data', t.notOk.bind(t));
+  setTimeout(function() {
+      ps.kill();
+  }, 3000)
+  ps.on('exit', function() {
+    t.end()
+  })
+  ps.stderr.on('data', function() {
+    t.fail()
+  })
 
   ps.stdin.write('console.log("foo");');
   ps.stdin.end();
