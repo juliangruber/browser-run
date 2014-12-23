7332e47a62c0be25cdd08c8f29d73be74fa3f306
Julian Gruber <julian@juliangruber.com>
2014-12-23 21:21:21 +0100

    reword

--- bin/bin.js
+++ bin/bin.js
@@ -18,9 +18,8 @@ var argv = optimist
   .describe('port', 'Starts listening on that port and waits for you to open a browser')
   .alias('p', 'port')
 
-  .describe('static', 'Serve static assets from directory specified in static option')
+  .describe('static', 'Serve static assets from this directory')
   .alias('s', 'static')
-
 
   .describe('help', 'Print help')
   .alias('h', 'help')
