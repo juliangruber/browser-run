148fe57e3884b4357652058f849f910bb7eb3316
Ryan Tsao <rtsao@uber.com>
2016-01-28 18:34:49 -0800

    use tree-kill to kill test spawned process

--- test/spawn.js
+++ test/spawn.js
@@ -1,12 +1,13 @@
 var test = require('tap').test;
 var spawn = require('child_process').spawn;
+var kill = require('tree-kill');
 
 test('spawn', function (t) {
   var ps = spawn(__dirname + '/../bin/bin.js');
   
   ps.stdout.on('data', function (data) {
     t.equal(data.toString(), 'foo\n');
-    ps.kill();
+    kill(ps.pid);
     t.end();
   });
   ps.stderr.on('data', t.notOk.bind(t));
